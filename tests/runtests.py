e78519e8df929a7a6c7bfd2dc6b8a0f02daee7c4
Mikko Hellsing <mikko@aino.se>
2011-05-21 22:40:50 +0200

    fresh tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
-from os.path import abspath, dirname, join as pjoin
 from django.conf import settings
+from os.path import abspath, dirname, join as pjoin
 
 
-def runtests(*test_labels):
+
+def runtests(test_labels, verbosity=1, interactive=True, failfast=True):
     here = abspath(dirname(__file__))
     root = pjoin(here, os.pardir)
     sys.path.append(root)
@@ -29,12 +30,41 @@ def runtests(*test_labels):
             },
             INSTALLED_APPS=labels,
         )
-    from django.test.simple import DjangoTestSuiteRunner
-    runner = DjangoTestSuiteRunner()
-    return runner.run_tests(test_labels)
+    from django.test.utils import get_runner
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=verbosity, interactive=interactive, failfast=failfast)
+    return test_runner.run_tests(test_labels)
 
 
 if __name__ == '__main__':
-    failures = runtests(*sys.argv[1:])
-    sys.exit(failures)
+    import argparse
+    parser = argparse.ArgumentParser(description='Runs the test suite for django-stringfield.')
+    parser.add_argument(
+        'test_labels',
+        nargs='*',
+        help='Test labels.',
+        )
+    parser.add_argument(
+        '--noinput',
+        dest='interactive',
+        action='store_false',
+        default=True,
+        help='Do not prompt the user for input of any kind.',
+        )
+    parser.add_argument(
+        '--failfast',
+        dest='failfast',
+        action='store_true',
+        default=False,
+        help='Stop running the test suite after first failed test.',
+    )
+    args = parser.parse_args()
+    failures = runtests(
+        args.test_labels,
+        verbosity=1,
+        interactive=args.interactive,
+        failfast=args.failfast
+        )
+    if failures:
+        sys.exit(bool(failures))
 
