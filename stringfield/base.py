f2d12cfcec172d8741dce223b84719f554c919c1
Mikko Hellsing <mikko@aino.se>
2011-11-16 12:05:35 +0100

    some fields w/validation

--- stringfield/base.py
+++ stringfield/base.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.core import validators
 from django.db import models
 from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext_lazy as _
@@ -51,4 +53,39 @@ class StringField(models.Field):
         args, kwargs = introspector(self)
         kwargs.pop('max_length', None)
         return name, args, kwargs
+
+
+class EmailField(StringField):
+    description = _("E-mail address")
+    default_validators = [validators.validate_email]
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': forms.EmailField}
+        defaults.update(kwargs)
+        return super(EmailField, self).formfield(**defaults)
+
+
+class URLField(StringField):
+    description = _("URL")
+
+    def __init__(self, *args, **kwargs):
+        verify_exists = kwargs.pop('verify_exists', False)
+        super(URLField, self).__init__(*args, **kwargs)
+        self.validators.append(validators.URLValidator(verify_exists=verify_exists))
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': forms.URLField}
+        defaults.update(kwargs)
+        return super(URLField, self).formfield(**defaults)
+
+
+class SlugField(StringField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('db_index', True)
+        super(SlugField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': forms.SlugField}
+        defaults.update(kwargs)
+        return super(SlugField, self).formfield(**defaults)
 
